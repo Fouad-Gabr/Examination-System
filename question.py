7a1a08a2fe7654c5cc71b7e6c5d2d84d5e6c123a
Fouad_Gabr14 <123542516+Fouad-Gabr@users.noreply.github.com>
2024-09-04 04:23:29 +0300

    Delete question.py

--- question.py
+++
@@ -1,9 +0,0 @@
-class Question():
-    pass
-    def __init__(self):
-        pass
-        self.question ="";
-        self.choice =[];
-        self.answer="";
-    def to_map(self):
-        return {"question":self.question, "choice":self.choice,"answer":self.answer};

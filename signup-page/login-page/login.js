aaa7057e103a02a7ed1c3cd3a9622427fb95c052
= <=>
2024-09-06 11:51:12 +0300

    make functionality at one class

--- signup-page/login-page/login.js
+++ signup-page/login-page/login.js
@@ -1,52 +1,5 @@
 "use strict";
+import Exam from "../../javascript_exam/Exam.js";
+window.obj1=new Exam();
+window.obj1=window.obj1.createLogInScreen()
 
-function goWelcomePage() {
-  if (logInValidate()) {
-    location.href = "../welcome-page/welcome.html";
-  }
-}
-
-function logInValidate() {
-  let username = document.getElementById("username");
-  let password = document.getElementById("password");
-  let userErr = document.getElementById("usernameErr");
-  let passErr = document.getElementById("passwordErr");
-  let logBtn = document.getElementById("loginBtn");
-
-  let f1 = false;
-  let f2 = false;
-
-  if (username.value.length == 0) {
-    userErr.style.display = "block";
-    userErr.innerHTML = `&#9888;Please enter your username`;
-  } else if (!(username.value == sessionStorage.getItem("email"))) {
-    userErr.style.display = "block";
-    userErr.innerHTML = `&#9888;an incorrect username`;
-  } else {
-    f1 = true;
-  }
-
-  if (password.value.length == 0) {
-    passErr.style.display = "block";
-    passErr.innerHTML = `&#9888;Please enter your password`;
-  } else if (!(password.value == sessionStorage.getItem("password"))) {
-    passErr.style.display = "block";
-    passErr.innerHTML = `&#9888;an incorrect password`;
-  } else {
-    f2 = true;
-  }
-
-  if (f1) {
-    userErr.innerHTML = "";
-  }
-
-  if (f2) {
-    passErr.innerHTML = "";
-  }
-
-  if (f1 && f2) {
-    logBtn.style.backgroundColor = "green";
-  }
-
-  return f1 && f2;
-}
